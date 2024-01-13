74933334b83476436147a9a37fc4e4c04e2c3830
Shohidullah <recursiveeducation@gmail.com>
2024-01-13 23:51:28 +0600

    React-setup

---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Menu(props) {
+    return (
+        <div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Menu;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from "../components/Menu";
+
+function Home(props) {
+    return (
+        <div>
+
+            <Menu />
+
+        </div>
+    );
+}
+
+export default Home;
